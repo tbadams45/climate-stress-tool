13393c98e04e1d10481c47b20744f81117f41ddb
Timothy Adams <tbadams45@gmail.com>
2017-06-29 11:52:33 -0400

    delete sim folder (used for model simulation, not weathergen)

--- public/src/sim/controllers/HomeController.js
+++
@@ -1,5 +0,0 @@
-
-angular.module('cst.sim')
-  .controller('HomeCtrl', ['$scope', '$state', function($scope, $state) {
-    $scope.message = 'Hello home';    
-  }]);
--- public/src/sim/controllers/SystemController.js
+++
@@ -1,5 +0,0 @@
-
-angular.module('cst.sim')
-  .controller('SystemCtrl', ['$scope', '$state', function($scope, $state) {
-    $scope.message = 'Hello system';    
-  }]);
